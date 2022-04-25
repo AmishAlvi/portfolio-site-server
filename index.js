ba685a2dff5e4ce7342015595f7d0fc48ea5b55f
AmishAlvi <amish.alvi@gmail.com>
2022-04-25 07:22:56 +0300

    changed placement of cors so that requests work

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ import mongoose from 'mongoose'
 import cors from 'cors'
 import postRoutes from './routes/posts.js'
 const app = express();
-
+app.use(cors());
 app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
-app.use(cors());
+
 
 const CONNECTION_URL = 'mongodb+srv://AmishAlvi1:AmishAlvi123@cluster0.qxl3q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
