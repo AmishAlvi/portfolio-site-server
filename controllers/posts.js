c15c8eb59ef900d11f3de59ce5a08295d6206988
AmishAlvi <amish.alvi@gmail.com>
2022-04-27 08:29:28 +0300

    added delete post functionality

--- controllers/posts.js
+++ controllers/posts.js
@@ -5,8 +5,6 @@ export const getPosts = async (req, res) => {
     try {
         const postMessages = await PostMessage.find();
 
-        console.log(postMessages);
-
         res.status(200).json(postMessages)
     } catch (error) {
         res.status(404).json({message: error.message})
@@ -31,7 +29,18 @@ export const updatePost = async (req, res) => {
     const {id: _id} = req.params;
     const post = req.body;
     if(!mongoose.Types.ObjectId.isValid(_id))  return res.status(404).send('No Post with that id');
-    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true})
+    
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, {...post, _id}, { new: true})
 
     res.json(updatedPost);
+}
+
+export const deletePost = async (req, res) => {
+    const {id} = req.params;
+    if(!mongoose.Types.ObjectId.isValid(id))  return res.status(404).send('No Post with that id');
+
+    console.log('DELETE!')
+    await PostMessage.findByIdAndRemove(id)
+
+    res.json({message: 'Post deleted succesfully'});
 }
