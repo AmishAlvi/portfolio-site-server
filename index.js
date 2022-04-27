84334af410cd1f81b61bf6771df2592eb7aa446d
AmishAlvi <amish.alvi@gmail.com>
2022-04-27 08:52:40 +0300

    hiding environment variables

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import postRoutes from './routes/posts.js'
+import dotenv from 'dotenv'
 const app = express();
+dotenv.config();
 
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
@@ -12,7 +14,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 
 
-const CONNECTION_URL = 'mongodb+srv://AmishAlvi1:AmishAlvi123@cluster0.qxl3q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL)
