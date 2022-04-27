ba337880e7bfbe840230b355c82b1288816acbd4
AmishAlvi <amish.alvi@gmail.com>
2022-04-27 09:30:09 +0300

    setting up for heroku deployment

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 app.use('/posts', postRoutes);
+app.use('/', (req, res) => {
+    res.send(`Welcome to Syed Amish Alvi's Portfolio API`)
+})
 
 
 const CONNECTION_URL = process.env.CONNECTION_URL;
