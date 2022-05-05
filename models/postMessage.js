539fa8b296138dd7023da5158c223119d036d8ab
AmishAlvi <amish.alvi@gmail.com>
2022-05-05 13:27:22 +0300

    added details to post schema

--- models/postMessage.js
+++ models/postMessage.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const postSchema = mongoose.Schema({
     title: String,
     description: String,
+    details: String,
     tags: [String],
     selectedFile: String,
     gitURL: String,
